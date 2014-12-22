34bcbae389cc81bf8e6f83c24b3a96f23117e4a9
Sung Won Cho <mikeswcho@gmail.com>
2014-12-22 15:39:06 +1100

    Fix typo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var redis = require('redis');
 if (process.env.REDISTOGO_URL) {
   var rtg = require('url').parse(process.env.REDISTOGO_URL);
   var client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtb.auth.split(":")[1]);
+  client.auth(rtg.auth.split(":")[1]);
 } else {
   var client = redis.createClient();
   client.select((process.env.NODE_ENV || 'development').length);
